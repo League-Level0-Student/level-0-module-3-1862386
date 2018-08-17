a8a72043f9b9bf3c2c585419d2c7ec7cdba4a757
League <league@WTS9.attlocal.net>
2018-08-16 19:56:11 -0700

    Walk of Fame, Fizz Buzz, and Bullseye

--- src/loops/WalkOfFame.java
+++ src/loops/WalkOfFame.java
@@ -11,8 +11,22 @@ public class WalkOfFame {
 		Robot rob = new Robot();
 		// 1. Set the X position of the robot so that it starts on the left. 
 		// You also need to show the robot to see the result of this line.
-
+   rob.setX(50);
 		// 2. Make the robot draw a star shape. Hint: 144.
+   rob.setSpeed(10);
+   rob.setRandomPenColor();
+   rob.turn(45);
+ rob.penDown();
+ rob.setPenWidth(30);
+rob.move(200);
+rob.turn(90);
+rob.move(200);
+rob.turn(150);
+rob.move(300);
+rob.turn(170);
+rob.move(300);
+rob.turn(170);
+rob.move(320);
 
 		// 3. Set the size of the star to 30.
 
